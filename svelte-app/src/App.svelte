fad7ff469201ce2cd09c7d489cfb2262eea4225a
Dr Richard Haddlesey <richard.haddlesey@sky.com>
2020-07-22 18:19:30 +0100

    reactive values

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,25 +1,38 @@
 <script>
-  let name = "Richie";
+  let firstName = "Richie";
+  let lastName = "H";
   let StateOfAwesomeness = "Very";
+  let colourOfAwesomeness = "blue";
+
+  $: fullName = `${firstName} ${lastName}`;
 
   const handleClick = () => {
     StateOfAwesomeness = "super";
+    colourOfAwesomeness = "green";
   };
 
   const handleInput = (e) => {
     StateOfAwesomeness = e.target.value
+  };
+
+  const handleInput2 = (e) => {
+    colourOfAwesomeness = e.target.value
   };
 </script>
 
 
 
 <main>
-  <h1>Hello {name}!</h1>
+  <h1>{fullName}</h1>
   <p>Level of awesomeness = {StateOfAwesomeness}</p>
+  <input type="text" bind:value={firstName} />
+  <input type="text" bind:value={lastName} />
+  <p style="color: {colourOfAwesomeness}">Colour of awesomeness = {colourOfAwesomeness}</p>
 
-  <button on:click={handleClick}>update awesomeness level</button>
+  <!-- <input type='text' on:input={handleInput} value={StateOfAwesomeness} /> -->
 
-  <input type='text' on:input={handleInput} value={StateOfAwesomeness} />
+  <input type="text" bind:value={StateOfAwesomeness} />
+  <input type="text" bind:value={colourOfAwesomeness} />
 
 </main>
 
