10daa995a1a44f69f5920ed8b32ca6b344948cbd
Dr Richard Haddlesey <richard.haddlesey@sky.com>
2020-07-22 12:49:10 +0100

    on:click added

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,30 +1,38 @@
 <script>
-	export let name;
+  let name = "Richie";
+  let StateOfAwesomeness = "Very";
+
+  const handleClick = () => {
+    StateOfAwesomeness = "super";
+  };
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>Level of awesomeness = {StateOfAwesomeness}</p>
+
+  <button on:click={handleClick}>update awesomeness level</button>
+
+</main>
