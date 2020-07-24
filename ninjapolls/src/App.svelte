d677a47e4fb39d230adb538df9fcaae98f5e5efb
Dr Richard Haddlesey <richard.haddlesey@sky.com>
2020-07-24 14:34:00 +0100

    added tabs

--- ninjapolls/src/App.svelte
+++ ninjapolls/src/App.svelte
@@ -1,12 +1,27 @@
 <script>
+import Header from "./components/Header.svelte";
+import Footer from "./components/Footer.svelte";
+import Tabs from "./shared/Tabs.svelte";
+
+// tabs
+	let items = ['Current Polls', 'Add New Poll'];
+	let activeItem = 'Current Polls';
+	const tabChange = (e) => {
+		activeItem = e.detail;
+	}
 
 </script>
 
+<Header />
 <main>
-	<h1>Hello Ninjas!</h1>
+<Tabs {activeItem} {items} on:tabChange={tabChange}/>
 	
 </main>
+<Footer/>
 
 <style>
-
+	main{
+		max-width: 960px;
+		margin: 40px auto;
+	}
 </style>
