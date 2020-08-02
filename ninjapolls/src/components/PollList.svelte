f3940521b112bff9d63efc344103b931e9a2b059
Dr Richard Haddlesey <richard.haddlesey@sky.com>
2020-08-02 19:11:21 +0100

    adding and styling poll cards

--- ninjapolls/src/components/PollList.svelte
+++ ninjapolls/src/components/PollList.svelte
@@ -1,4 +1,5 @@
 <script>
+   import PollDetails from "./PollDetails.svelte";
   export let polls = [];
 </script>
 
@@ -12,6 +13,6 @@
 
 <div class="poll-list">
   {#each polls as poll (poll.id)}
-    <div>{poll.question}</div>
+    <PollDetails {poll} />
   {/each}
 </div>
