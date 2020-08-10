ff42ef0920f3bb01bc52c52e90a54dfeb9e9f7cf
Dr Richard Haddlesey <richard.haddlesey@sky.com>
2020-08-10 13:41:37 +0100

    messAroundApp

---
+++ udemy/first-app/src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	let name = 'Richie';
+	let age = 21;
+
+	$: uppercaseName = name.toUpperCase();
+
+	function incrementAge() {
+		age += 1;
+	}
+
+	function changeName() {
+		name = "Richard";
+	}
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {uppercaseName}, my age is {age}!</h1>
+<button on:click="{incrementAge}">Change Age</button>
+<button on:click="{changeName}">Change Name</button>
